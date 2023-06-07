5b55a54e9e3ecddbef9167813e7f91b71dede539
bobpulaski <82018695+bobpulaski@users.noreply.github.com>
2023-06-07 14:33:17 +0300

    first commit

---
+++ src/pages/about.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "gatsby";
+
+function about() {
+  return (
+    <>
+      <div>about</div>
+      <Link to="/">home</Link>
+    </>
+  );
+}
+
+export default about;
